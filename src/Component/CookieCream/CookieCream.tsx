0e42f61bf579a721825971f35081c25ecccb20e1
iinoten <nteten0313@icloud.com>
2023-07-02 20:30:33 +0900

    Add クッキーとクリーム部分を描画するコンポーネントファイルを設定

---
+++ src/Component/CookieCream/CookieCream.tsx
@@ -0,0 +1,10 @@
+import { color } from "@/constant"
+import "./CookieCream.css"
+
+const addStyle = (isCookie = false) => {
+    
+}
+
+export const CookieCream = () => (
+    <div className="Cookie"  />
+)
